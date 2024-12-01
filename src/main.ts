29648d088d2896a6e9b14d6449658cde556e19bb
Elbert Nathanael <elbertwithane@gmail.com>
2024-12-01 18:48:42 +0800

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,121 @@
-import './style.css'
-import { InitiailiseRive, SetRiveLock } from './rive'
+import "./style.css";
+import { InitiailiseRive, SetRiveLock } from "./rive";
 
+//intefaces
+interface UserData {
+  lastLocked: number;
+  locked: boolean;
+}
 
 // props and states
 const constants = {
-    localStorageKey: "didILock",
-}
+  localStorageKey: "didILock",
+  durationToUnlock: 1000 * 60 * 20, // 1s * 60 & 20 (20 mins)
+  greenColor: "72FFC2",
+  redColor: "FF7E7E",
+};
 let locked = false;
 
+// init rive first
+InitiailiseRive(
+  document.getElementById("canvas") as HTMLCanvasElement,
+  document.querySelector(".canvasWrapper") as HTMLElement
+);
 
 // main functions
+let userData: UserData | null = null;
 function onInitialLoad() {
-    // init rive
-    InitiailiseRive(
-        document.getElementById("canvas") as HTMLCanvasElement,
-        document.querySelector(".canvasWrapper") as HTMLElement
-    )
-    // load prev data
+  // this is the main start function
+
+  // load prev data
+  const data = loadData();
+  // create default data if not there
+  if (data === null) {
+    const data: UserData = { lastLocked: 0, locked: false };
+    saveData(data);
+    userData = data;
+  } else {
+    // load the data if not null
+    userData = loadData();
+  }
+
+  // check if elapsed time has caused the chain to lock
+  const duration = Date.now() - userData!.lastLocked;
+  if (duration > constants.durationToUnlock) {
+    setLockFalse();
+  } else {
+    setLockTrue();
+  }
 }
 function setLockTrue() {
-    // set vars
-    locked = true
-    // log the time
-    // toggle graphics
-    SetRiveLock(true)
-    // todo css backgrounds
-    // update ui text
+  // set vars
+  locked = true;
+  userData!.locked = true;
+  // log the time
+  userData!.lastLocked = Date.now();
+  // save data
+  saveData(userData!);
+  // toggle graphics
+  SetRiveLock(true);
+  // css backgrounds
+  document.body.setAttribute(
+    "style",
+    `background-color:#${constants.greenColor}`
+  );
+  // update ui text
+  document.getElementById("answer")!.innerHTML = "YES!";
+  updateDurationUI();
 }
 function setLockFalse() {
-    // set vars
-    locked = false
-    // log the time
-    // toggle graphics
-    SetRiveLock(false)
-    // todo css backgrounds
-    // update ui text
+  // set vars
+  locked = false;
+  userData!.locked = false;
+  // save data
+  saveData(userData!);
+  // toggle graphics
+  SetRiveLock(false);
+  // css backgrounds
+  document.body.setAttribute(
+    "style",
+    `background-color:#${constants.redColor}`
+  );
+  // update ui text
+  document.getElementById("answer")!.innerHTML = "NO!";
+  updateDurationUI();
 }
-
-onInitialLoad()
-
+function saveData(data: UserData) {
+  localStorage.setItem(constants.localStorageKey, JSON.stringify(data));
+}
+function loadData() {
+  return JSON.parse(
+    localStorage.getItem(constants.localStorageKey)!
+  ) as UserData | null;
+}
+function updateDurationUI() {
+  // get minutes
+  const duration = Date.now() - userData!.lastLocked;
+  let durationInMinutes = Math.round(duration / 1000 / 60);
+  // clamp duration
+  if (durationInMinutes > constants.durationToUnlock) {
+    durationInMinutes = Math.round((constants.durationToUnlock * 1000) / 60);
+  }
+  if (durationInMinutes < 1) {
+    durationInMinutes = 1;
+  }
+  // update ui
+  document.getElementById(
+    "durationLabel"
+  )!.innerHTML = `Last locked about ${durationInMinutes} minute(s) ago`;
+}
 
 // event callbacks
 document.getElementById("canvas")?.addEventListener("click", () => {
-    if (locked) {
-        setLockFalse()
-    } else {
-        setLockTrue()
-    }
-})
+  if (locked) {
+    setLockFalse();
+  } else {
+    setLockTrue();
+  }
+});
+
+//! note lol this is a bit backwards but you start rive first then... you start your logic
+export default onInitialLoad;
