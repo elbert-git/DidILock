c9ab9ea146b3d9f3108ccfa8c9250f44c1c4dfbb
Elbert Nathanael <elbertwithane@gmail.com>
2024-12-05 00:15:12 +0800

    fixed resize and delayed check

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { InitiailiseRive, SetRiveLock } from "./rive";
 
 //intefaces
 interface UserData {
-  lastLocked: number;
-  locked: boolean;
+    lastLocked: number;
+    locked: boolean;
 }
 
 // props and states
 const constants = {
-  localStorageKey: "didILock",
-  durationToUnlock: 1000 * 60 * 20, // 1s * 60 & 20 (20 mins)
-  greenColor: "72FFC2",
-  redColor: "FF7E7E",
+    localStorageKey: "didILock",
+    durationToUnlock: 1000 * 60 * 20, // 1s * 60 & 20 (20 mins)
+    greenColor: "72FFC2",
+    redColor: "FF7E7E",
 };
 let locked = false;
 
@@ -22,105 +22,109 @@ InitiailiseRive(document.getElementById("canvas") as HTMLCanvasElement);
 // main functions
 let userData: UserData | null = null;
 function onInitialLoad() {
-  // this is the main start function
+    // this is the main start function
 
-  // load prev data
-  const data = loadData();
-  // create default data if not there
-  if (data === null) {
-    const data: UserData = { lastLocked: 0, locked: false };
-    saveData(data);
-    userData = data;
-  } else {
-    // load the data if not null
-    userData = loadData();
-  }
-
-  // if is unlocked previously leave it unlocked
-  if (userData!.locked === false) {
-    setLockFalse();
-  }
-  // if is locked previously
-  else {
-    // if longer than threshold, make unlock
-    const duration = Date.now() - userData!.lastLocked;
-    if (duration > constants.durationToUnlock) {
-      setLockFalse();
+    // load prev data
+    const data = loadData();
+    // create default data if not there
+    if (data === null) {
+        const data: UserData = { lastLocked: 0, locked: false };
+        saveData(data);
+        userData = data;
+    } else {
+        // load the data if not null
+        userData = loadData();
     }
-    // if locked within threshold leave locked
+
+    // if is unlocked previously leave it unlocked
+    if (userData!.locked === false) {
+        setLockFalse();
+    }
+    // if is locked previously
     else {
-      setLockTrue();
+        // if longer than threshold, make unlock
+        const duration = Date.now() - userData!.lastLocked;
+        if (duration > constants.durationToUnlock) {
+            setLockFalse();
+        }
+        // if locked within threshold leave locked
+        else {
+            setLockTrue();
+        }
     }
-  }
+    //  this is dumb but it will work to automatically the ui
+    setInterval(() => {
+        updateDurationUI()
+    }, 1000)
 }
 function setLockTrue() {
-  // set vars
-  locked = true;
-  userData!.locked = true;
-  // log the time
-  userData!.lastLocked = Date.now();
-  // save data
-  saveData(userData!);
-  // toggle graphics
-  SetRiveLock(true);
-  // css backgrounds
-  document.body.setAttribute(
-    "style",
-    `background-color:#${constants.greenColor}`
-  );
-  // update ui text
-  document.getElementById("answer")!.innerHTML = "YES!";
-  updateDurationUI();
+    // set vars
+    locked = true;
+    userData!.locked = true;
+    // log the time
+    userData!.lastLocked = Date.now();
+    // save data
+    saveData(userData!);
+    // toggle graphics
+    SetRiveLock(true);
+    // css backgrounds
+    document.body.setAttribute(
+        "style",
+        `background-color:#${constants.greenColor}`
+    );
+    // update ui text
+    document.getElementById("answer")!.innerHTML = "YES!";
+    updateDurationUI();
 }
 function setLockFalse() {
-  // set vars
-  locked = false;
-  userData!.locked = false;
-  // save data
-  saveData(userData!);
-  // toggle graphics
-  SetRiveLock(false);
-  // css backgrounds
-  document.body.setAttribute(
-    "style",
-    `background-color:#${constants.redColor}`
-  );
-  // update ui text
-  document.getElementById("answer")!.innerHTML = "NO!";
-  updateDurationUI();
+    // set vars
+    locked = false;
+    userData!.locked = false;
+    // save data
+    saveData(userData!);
+    // toggle graphics
+    SetRiveLock(false);
+    // css backgrounds
+    document.body.setAttribute(
+        "style",
+        `background-color:#${constants.redColor}`
+    );
+    // update ui text
+    document.getElementById("answer")!.innerHTML = "NO!";
+    updateDurationUI();
 }
 function saveData(data: UserData) {
-  localStorage.setItem(constants.localStorageKey, JSON.stringify(data));
+    localStorage.setItem(constants.localStorageKey, JSON.stringify(data));
 }
 function loadData() {
-  return JSON.parse(
-    localStorage.getItem(constants.localStorageKey)!
-  ) as UserData | null;
+    return JSON.parse(
+        localStorage.getItem(constants.localStorageKey)!
+    ) as UserData | null;
 }
 function updateDurationUI() {
-  // get minutes
-  const duration = Date.now() - userData!.lastLocked;
-  let durationInMinutes = Math.round(duration / 1000 / 60);
-  // clamp duration
-  if (durationInMinutes > constants.durationToUnlock) {
-    durationInMinutes = Math.round((constants.durationToUnlock * 1000) / 60);
-  }
-  if (durationInMinutes < 1) {
-    durationInMinutes = 1;
-  }
-  // update ui
-  document.getElementById(
-    "durationLabel"
-  )!.innerHTML = `Last locked about ${durationInMinutes} minute(s) ago`;
+    // get minutes
+    const duration = Date.now() - userData!.lastLocked;
+    let durationInMinutes = Math.round(duration / 1000 / 60);
+    // clamp duration
+    if (durationInMinutes > constants.durationToUnlock) {
+        durationInMinutes = Math.round((constants.durationToUnlock * 1000) / 60);
+    }
+    if (durationInMinutes < 1) {
+        durationInMinutes = 1;
+    }
+    // update ui
+    document.getElementById(
+        "durationLabel"
+    )!.innerHTML = `Last locked about ${durationInMinutes} minute(s) ago`;
 }
 
 // event callbacks
 document.getElementById("canvas")?.addEventListener("click", () => {
-  if (locked) {
-    setLockFalse();
-  } else {
-    setLockTrue();
-  }
+    if (locked) {
+        setLockFalse();
+    } else {
+        setLockTrue();
+    }
 });
 
 //! note lol this is a bit backwards but you start rive first then... you start your logic
--- src/rive.ts
+++ src/rive.ts
@@ -4,50 +4,55 @@ import onInitialLoad from "./main";
 let lockBool: rive.StateMachineInput | undefined = undefined;
 
 export function InitiailiseRive(canvas: HTMLCanvasElement) {
-  // create on resize
-  function onResize() {
-    // const parentWidth = parent.clientWidth
-    // const parentHeight = parent.clientHeight
-    // const sizeMultiplier = 3
-    // const size = (parent.clientHeight * sizeMultiplier) / 2
-    const size = 450 / 2;
-    // set canvas size
-    canvas.height = size;
-    canvas.width = size;
-    r.resizeDrawingSurfaceToCanvas();
-  }
+    // create on resize
+    function onResize() {
+        // const parentWidth = parent.clientWidth
+        // const parentHeight = parent.clientHeight
+        // const sizeMultiplier = 3
+        // const size = (parent.clientHeight * sizeMultiplier) / 2
+        const size = 450 / 2;
+        // set canvas size
+        canvas.height = size;
+        canvas.width = size;
+        r.resizeDrawingSurfaceToCanvas();
+    }
 
-  // init the rive object
-  const r = new rive.Rive({
-    src: "lock.riv",
-    // OR the path to a discoverable and public Rive asset
-    // src: '/public/example.riv',
-    canvas: canvas,
-    autoplay: true,
-    // artboard: "Arboard", // Optional. If not supplied the default is selected
-    stateMachines: "states",
-    onLoad: () => {
-      console.log("loaded");
-      // resize the canvas on start
-      onResize();
-      // get state machinse inputs
-      const stateMachinesInputs = r.stateMachineInputs("states");
-      // get lock input
-      lockBool = stateMachinesInputs.find((i) => i.name === "lock");
-      //! note lol this is a bit backwards but you start rive first then... you start your logic
-      //execute start up
-      onInitialLoad();
-    },
-  });
+    // init the rive object
+    const r = new rive.Rive({
+        src: "lock.riv",
+        // OR the path to a discoverable and public Rive asset
+        // src: '/public/example.riv',
+        canvas: canvas,
+        autoplay: true,
+        // artboard: "Arboard", // Optional. If not supplied the default is selected
+        stateMachines: "states",
+        onLoad: () => {
+            console.log("loaded");
+            // resize the canvas on start
+            onResize();
+            // get state machinse inputs
+            const stateMachinesInputs = r.stateMachineInputs("states");
+            // get lock input
+            lockBool = stateMachinesInputs.find((i) => i.name === "lock");
+            //! note lol this is a bit backwards but you start rive first then... you start your logic
+            //execute start up
+            onInitialLoad();
+        },
+    });
 
-  onResize();
-
-  // listen to futuer rive events
-  window.addEventListener("resize", () => {
     onResize();
-  });
+
+    // listen to futuer rive events
+    window.addEventListener("resize", () => {
+        onResize();
+    });
+
+    //  this is dumb but it will work to automatically resize
+    setInterval(() => {
+        onResize()
+    }, 1000)
 }
 
 export function SetRiveLock(b: boolean) {
-  lockBool!.value = b;
+    lockBool!.value = b;
 }
